07db3d384170b7c61bfb1b89f005a25205f51339
richardsonadjei <adjei.kwadjo@gmail.com>
2023-10-09 11:02:29 +0000

    Create Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Profile from './pages/Profile';
-import SignIn from  './pages/SignIn';
+import SignIn from './pages/SignIn';
 import SignOut from './pages/SignOut';
-import SignUp from './pages/SignUp'
+import SignUp from './pages/SignUp';
+import Header from '../src/components/Header';
+
 function App() {
-  return(
-   <BrowserRouter>
-    <Routes>
-      <Route path='/about' element={<About />}/> 
-      <Route path='/home' element={<Home />}/> 
-      <Route path='/signin' element={<SignIn />}/> 
-      <Route path='/signout' element={<SignOut />}/> 
-      <Route path='/signup' element={<SignUp />}/> 
-      <Route path='/profile' element={<Profile />}/> 
-    </Routes>  
-  </BrowserRouter>
-  )
+  return (
+    <BrowserRouter>
+      <Header /> 
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signout" element={<SignOut />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaLeaf } from 'react-icons/fa';
+
+export default function Header() {
+  return (
+    <div className="bg-gray-800">
+      <header className="container mx-auto py-4 flex justify-between items-center">
+        <div className="flex items-center">
+          <Link to="/about">
+            <FaLeaf className="text-green-500 text-2xl mr-2" />
+          </Link>
+          <div className="text-white text-xl font-bold">
+            <Link to="/about" className="text-white hover:text-gray-300">
+              Pador Farms Franchise
+            </Link>
+          </div>
+        </div>
+        <nav className="mt-4 lg:mt-0">
+          <ul className="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-4">
+            <li>
+              <Link to="/about" className="text-white hover:text-gray-300">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/signin" className="text-white hover:text-gray-300">
+                SignIn
+              </Link>
+            </li>
+            <li>
+              <Link to="/signout" className="text-white hover:text-gray-300">
+                SignOut
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
