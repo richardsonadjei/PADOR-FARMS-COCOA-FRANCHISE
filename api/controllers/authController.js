4e28f1474bad668cce784d7634741e3d26b0c2fe
richardsonadjei <adjei.kwadjo@gmail.com>
2023-10-09 16:38:25 +0000

    Create SignIn API route And Authenticate With Cook

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { errorHandler } = require('../middleware/errorHandler');
 
 // Controller function for user signup
@@ -33,5 +34,52 @@ exports.signup = async (req, res, next) => {
   }
 };
 
+// Controller function for user signin
+exports.signin = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    // Find the user by username
+    const user = await User.findOne({ username });
+    if (!user) {
+      const error = new Error('Invalid username or password');
+      error.statusCode = 401;
+      throw error;
+    }
+    // Compare the hashed password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      const error = new Error('Invalid username or password');
+      error.statusCode = 401;
+      throw error;
+    }
+    // Generate a token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '8h',
+    });
+    // Set the token as a httpOnly cookie
+    res.cookie('token', token, { httpOnly: true, maxAge: 8 * 60 * 60 * 1000 });
+    // Return success response
+    return res.status(200).json({ user: { 
+      fullname: user.fullname,
+      username: user.username,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      bio: user.bio
+    }});
+  } catch (error) {
+    next(error);
+  }
+};
+
 // Error handling middleware
 exports.handleErrors = errorHandler;
+
+// Error handler middleware
+exports.errorHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const success = false;
+  const message = err.message || 'Server error';
+  console.error(err);
+  // Send the error response to the client
+  res.status(statusCode).json({ success, statusCode, message });
+};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/authController');
 
 // POST route for user signup
 router.post('/signup', userController.signup);
+router.post('/signin', userController.signin);
 
 module.exports = router;
