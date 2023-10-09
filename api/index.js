5f584f419fd7d2e49a342c175215cdf9bc92a386
richardsonadjei <adjei.kwadjo@gmail.com>
2023-10-09 11:56:49 +0000

    connecting to database

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 const app = express();
 const port = 3000; 
 
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
+
