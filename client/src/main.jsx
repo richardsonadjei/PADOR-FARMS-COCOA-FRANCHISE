3c895322fcfa8b92e824b9a12937edaf6378fc7f
richardsonadjei <adjei.kwadjo@gmail.com>
2023-10-09 21:45:49 +0000

    Adding Redux Persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'; // Import createRoot from 'react-dom/client'
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.jsx';
 import './index.css';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
 
-const root = document.getElementById('root');
-const reactRoot = createRoot(root);
+const reactRoot = createRoot(document.getElementById('root'));
 
 reactRoot.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,16 +1,34 @@
-
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from '../redux/user/userSlice';
+// ... import other reducers as needed
 
+// Combine your reducers into a single rootReducer
+const rootReducer = combineReducers({
+  user: userReducer,
+  // ... add other reducers here
+});
+
+// Configure the persist options
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+// Create the persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create the Redux store
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
 
+// Create the persisted store
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
